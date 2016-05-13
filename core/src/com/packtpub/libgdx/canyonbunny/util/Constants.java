311dda554f450c1419789e5dd71067467cc5c83b
leandro.silverio <leandro.silverio@lge.com>
2016-05-13 18:17:17 -0300

    First Blood
    Signed-off-by: leandro.silverio <leandro.silverio@lge.com>

---
+++ core/src/com/packtpub/libgdx/canyonbunny/util/Constants.java
@@ -0,0 +1,10 @@
+package com.packtpub.libgdx.canyonbunny.util;
+
+/**
+ * Created by leandro.silverio on 12/05/2016.
+ */
+public class Constants {
+    public static final float VIEWPORT_WIDTH = 5.0f;
+
+    public static final float VIEWPORT_HEIGHT = 5.0f;
+}
